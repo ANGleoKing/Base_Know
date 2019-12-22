f13d79a06682fc70bb8dfcf83d4f20979b4ce759
ANGleKing <2655258260@qq.com>
2019-12-22 23:57:54 +0800

    栈，队列，串的简单操作

---
+++ src/main/java/Java_Base/Collection/Map.java
@@ -0,0 +1,27 @@
+package Java_Base.Collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * @ClassNameMap
+ * @Description
+ * @Author ANGLE0
+ * @Date2019/12/20 18:35
+ * @Version V1.0
+ **/
+public class Map extends HashMap {
+
+
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String >();
+         map.put("name", "lili");
+         map.put("age", "12");
+         map.put("address", "zg");
+
+        System.out.println(map.get("name"));
+        System.out.println(map.get("age"));
+    }
+
+}
