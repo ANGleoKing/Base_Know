4339cc7283f8f1e830a2693e2f5f81e99e7494c6
ANGleKing <2655258260@qq.com>
2020-05-05 21:07:43 +0800

    add explain

--- src/main/java/com/w/algorithm/leetcode/rmElement.java
+++ src/main/java/com/w/algorithm/leetcode/rmElement.java
@@ -14,18 +14,28 @@ public class rmElement {
         int index = 0;
 
         while (index <= len-1-count){
+            /**
+             * 从前向后找到第一个[==val]的数的索引
+             * index < len，避免越界
+             */
             while (index < len && nums[index] != val){
                 index++;
             }
+            /**从后向前找到第一个[!=val]的数的索引
+             * count < len，避免越界
+             */
             while (count < len && nums[len-1-count] == val){
                 count++;
             }
+            //相等,则当前数组中没有可保留的值
             if (count == len)
                 return 0;
+            //index < len-1-count,数组未找完，且当前可交换
             if (index < len-1-count){
                 swap(nums, index, len-1-count);
             }
         }
+        //当前有效数据个数=原长度[len]-删除个数[count]
         return len-count;
     }
 
