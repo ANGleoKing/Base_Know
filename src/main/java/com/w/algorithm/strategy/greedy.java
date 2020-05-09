f394433ac3bc4f82ea6434019d0b5635a4ceda40
ANGleKing <2655258260@qq.com>
2020-05-09 22:17:24 +0800

    algorithm add files

---
+++ src/main/java/com/w/algorithm/strategy/greedy.java
@@ -0,0 +1,12 @@
+package com.w.algorithm.strategy;
+
+/**
+ * @ClassName greedy
+ * @Description [贪心算法]
+ * @Author ANGLE0
+ * @Date 2020/5/9 13:18
+ * @Version V1.0
+ **/
+public class greedy {
+    
+}
